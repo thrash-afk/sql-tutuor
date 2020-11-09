90f9e9a3c3191292ec479074ed37035ed863abae
thrash-afk <i.koshkaroff@yandex.ru>
2020-11-09 21:03:51 +0700

    Add create_table.sql. Craete tables publisher and book

---
+++ create_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE publisher
+(
+	publisher_id integer PRIMARY KEY,
+	org_name varchar(128) NOT NULL,
+	address text NOT NULL
+);
+
+CREATE TABLE book
+(
+	book_id integer PRIMARY KEY,
+	title text NOT NULL,
+	isbn varchar(32) NOT NULL
+)
