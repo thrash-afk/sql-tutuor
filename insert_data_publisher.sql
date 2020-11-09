91dc5e0a98fba2815f2de1bcc730ce4d94ea4dc7
thrash-afk <i.koshkaroff@yandex.ru>
2020-11-09 21:13:27 +0700

    Create insert_data_publisher.sql
    Add file insert_data_publisher. Insert fake data into table publisher

---
+++ insert_data_publisher.sql
@@ -0,0 +1,6 @@
+INSERT INTO publisher
+VALUES
+(1, 'Everyman''s Library', 'NY'),
+(2, 'Oxford University Press', 'NY'),
+(3, 'Grand Central Publishing', 'Washington'),
+(4, 'Simon & Schuster', 'Chicago')
