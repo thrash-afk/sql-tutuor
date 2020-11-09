01a7ba208779c5390ec9a83def6590e6a3fd99a7
thrash-afk <i.koshkaroff@yandex.ru>
2020-11-09 21:57:07 +0700

    Create insert_data_book.sql
    Insertfake data into table book

---
+++ insert_data_book.sql
@@ -0,0 +1,7 @@
+INSERT INTO book
+VALUES
+(1, 'The Diary of a Young Girl', '0199535566', 1),
+(2, 'Pride and Prejudice', '9780307594006', 1),
+(3, 'To Kill a Mockingbird', '0446310786', 2),
+(4, 'The Book of Gutsy Women: Favorite Stories of Courage and Resiliense', '1501178415', 2),
+(5, 'War and Peace', '1788886525', 2)
