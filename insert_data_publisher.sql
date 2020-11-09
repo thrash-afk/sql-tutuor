ea58f583cab496b8fcc380aead389aef207c9b19
thrash-afk <i.koshkaroff@yandex.ru>
2020-11-09 21:26:35 +0700

    Revert "Create insert_data_publisher.sql"
    This reverts commit 91dc5e0a98fba2815f2de1bcc730ce4d94ea4dc7.

--- insert_data_publisher.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO publisher
-VALUES
-(1, 'Everyman''s Library', 'NY'),
-(2, 'Oxford University Press', 'NY'),
-(3, 'Grand Central Publishing', 'Washington'),
-(4, 'Simon & Schuster', 'Chicago')
